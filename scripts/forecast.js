2fa516932ef1477d3e5af933c7d137c042a601ac
Prakhar Jain <jainprak007@gmail.com>
2023-02-21 13:43:30 +0530

    weather_project

---
+++ scripts/forecast.js
@@ -0,0 +1,33 @@
+const key='	UnKuWEIVAJwEeaGGevlgnnc0EwaWmNgy';
+
+const getWeather=async(id)=>{
+    const base='http://dataservice.accuweather.com/currentconditions/v1/';
+    const query=`${id}?apikey=${key}`;
+
+    const response=await fetch(base+query);
+    const data=await response.json();
+
+    return data[0];
+}
+
+const getCity= async(city)=>{
+    const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query=`?apikey=${key}&q=${city}`;
+
+    const response=await fetch(base+query);
+    const data=await response.json();
+
+    return(data[0]);
+
+};
+
+// getCity('Firozabad')
+// .then(data=>{
+//     return getWeather(data.Key);
+// }).then(data=>{
+//     console.log(data);
+// })
+// .catch(err=>console.log(err));
+
+// getWeather("191034").then(data=>console.log(data))
+// .catch(err=>console.loglog(err));
