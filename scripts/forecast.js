452438fede77a5ab927c81b158b33406d5e9e0ec
Prakhar Jain <jainprak007@gmail.com>
2023-02-21 14:23:01 +0530

    change2

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -14,9 +14,7 @@ const getCity= async(city)=>{
     const base ='http://dataservice.accuweather.com/locations/v1/cities/search';
     const query=`?apikey=${key}&q=${city}`;
 
-    const response=await fetch(base+query, {
-        referrerPolicy: "unsafe_url" 
-    });
+    const response=await fetch(base+query);
     const data=await response.json();
 
     return(data[0]);
